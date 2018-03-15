9b3d539d930efe3b0ce994cd3c1132ab702b1fb6
Yuchi <yuchiw@andrew.cmu.edu>
2018-03-15 18:14:44 -0400

    master: initial commit

---
+++ code/SimpleEnvironment.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pylab as pl
+import itertools
+from DiscreteEnvironment import DiscreteEnvironment
+
+class SimpleEnvironment(object):
+    
+    def __init__(self, herb, resolution):
+        self.robot = herb.robot
+        self.lower_limits = [-5., -5.]
+        self.upper_limits = [5., 5.]
+        self.discrete_env = DiscreteEnvironment(resolution, self.lower_limits, self.upper_limits)
+
+        # add an obstacle
+        table = self.robot.GetEnv().ReadKinBodyXMLFile('models/objects/table.kinbody.xml')
+        self.robot.GetEnv().Add(table)
+
+        table_pose = np.array([[ 0, 0, -1, 1.5], 
+                                  [-1, 0,  0, 0], 
+                                  [ 0, 1,  0, 0], 
+                                  [ 0, 0,  0, 1]])
+        table.SetTransform(table_pose)
+
+    def GetSuccessors(self, node_id):
+
+        grid_coord = self.discrete_env.NodeIdToGridCoord(node_id)
+        offset = np.zeros(grid_coord.shape)
+        offset[0] = 1
+
+        return np.array([self.discrete_env.GridCoordToNodeId(grid_coord + off) for 
+            off in [-p, p] for p in itertools.permutations(offset)])
+
+    def ComputeDistance(self, start_id, end_id):
+
+        return numpy.linalg.norm(self.discrete_env.NodeIdToConfiguration(start_id)
+            -self.discrete_env.NodeIdToConfiguration(end_id))
+
+    def ComputeHeuristicCost(self, start_id, goal_id):
+        #Use distance as heuristic?
+        return numpy.linalg.norm(self.discrete_env.NodeIdToConfiguration(start_id)
+            -self.discrete_env.NodeIdToConfiguration(end_id))        
+
+    def InitializePlot(self, goal_config):
+        self.fig = pl.figure()
+        pl.xlim([self.lower_limits[0], self.upper_limits[0]])
+        pl.ylim([self.lower_limits[1], self.upper_limits[1]])
+        pl.plot(goal_config[0], goal_config[1], 'gx')
+
+        # Show all obstacles in environment
+        for b in self.robot.GetEnv().GetBodies():
+            if b.GetName() == self.robot.GetName():
+                continue
+            bb = b.ComputeAABB()
+            pl.plot([bb.pos()[0] - bb.extents()[0],
+                     bb.pos()[0] + bb.extents()[0],
+                     bb.pos()[0] + bb.extents()[0],
+                     bb.pos()[0] - bb.extents()[0],
+                     bb.pos()[0] - bb.extents()[0]],
+                    [bb.pos()[1] - bb.extents()[1],
+                     bb.pos()[1] - bb.extents()[1],
+                     bb.pos()[1] + bb.extents()[1],
+                     bb.pos()[1] + bb.extents()[1],
+                     bb.pos()[1] - bb.extents()[1]], 'r')
+                    
+                     
+        pl.ion()
+        pl.show()
+        
+    def PlotEdge(self, sconfig, econfig):
+        pl.plot([sconfig[0], econfig[0]],
+                [sconfig[1], econfig[1]],
+                'k.-', linewidth=2.5)
+        pl.draw()
+
+        
